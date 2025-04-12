beb7ff545252009fbf57b9349bd23ec60a84ea80
santosh226206 <santoshamcl1996@gmail.com>
2025-04-12 11:20:20 +0530

    initial commits

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello from java jenkins test");
+        System.out.println("ambustement");
+        System.out.println("ktklatomos");
+        System.out.println("yaad rkhne ka");
     }
 }
