490adc568ce9247f1aa6ac2470be7fed3dd6cb5c
santosh226206 <santoshamcl1996@gmail.com>
2025-04-12 11:31:24 +0530

    initial commits

--- Main.java
+++ Main.java
@@ -4,5 +4,8 @@ public class Main {
         System.out.println("ambustement");
         System.out.println("ktklatomos");
         System.out.println("yaad rkhne ka");
+        System.out.println("java is good language");
+        System.out.println("you can create scalable backend");
+        System.out.println("with spring boot it is become very easy to build microservice");
     }
 }
